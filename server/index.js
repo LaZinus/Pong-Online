1bd14f9bfd57306fc38f630ebc95cea5bba652f0
lazinus <lazinus67@gmail.com>
2024-08-25 09:25:35 +0200

    done

--- server/index.js
+++ server/index.js
@@ -16,9 +16,13 @@ const expressServer = app.listen(PORT, () => {
     console.log(`[Server] listening on ${PORT}`);
 });
 
-const io = new Server(expressServer)
+const io = new Server(expressServer, {
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
+    }
+});
 
 io.on('connection', socket => {
-    console.log(`User ${socket.id} connevted`);
+    console.log(`User ${socket.id} connected`);
 });
 
