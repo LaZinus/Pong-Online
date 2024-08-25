ae34884fadb9f3913564c37c29aef2490c18a6b7
lazinus <lazinus67@gmail.com>
2024-08-25 11:50:03 +0200

    connect client to server;

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 const app = express();
 
--- server/public/app.js
+++ server/public/app.js
@@ -1,1 +1,1 @@
-const socket = io('ws://localhost:3500');
+const socket = io('ws://127.0.0.1:8080');
